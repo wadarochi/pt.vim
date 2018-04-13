dd8f6a9179d1d7570ddcc5fa383dd2636d01e718
wadarochi <pt0079@gmail.com>
2018-04-13 22:27:02 +0800

    添加vimwiki主题，并修改安装脚本

--- script/setup_vim_freebsd.sh
+++ script/setup_vim_freebsd.sh
@@ -10,6 +10,13 @@ mkdir -p ~/.vim/swap
 mkdir -p ~/.vim/bundle/
 cp -R pt.vim ~/.vim/bundle/pt.vim
 
+# vimwiki html theme
+mkdir -p ~/vimwiki
+mkdir -p ~/vimwiki_html/assets
+mkdir -p ~/vimwiki_html/template
+cp -R assets/* ~/vimwiki_html/assets/
+cp -R template/* ~/vimwiki_html/template/
+
 # install dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.vim/dein/
