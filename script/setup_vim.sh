93917a4e74b4632da6c2e98361ed4d28bfeff742
Zhang Binbin <wadarochi@gmail.com>
2018-08-29 20:36:32 +0800

    Windows platform compatibility

--- script/setup_vim.sh
+++ script/setup_vim.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-cp vimrc ~/.vimrc
+cp vimrc /mnt/c/Users/gzzhangbinbin/.vimrc
 
-mkdir -p ~/.vim/undo
-mkdir -p ~/.vim/backup
-mkdir -p ~/.vim/swap
+mkdir -p /mnt/c/Users/gzzhangbinbin/.vim/undo
+mkdir -p /mnt/c/Users/gzzhangbinbin/.vim/backup
+mkdir -p /mnt/c/Users/gzzhangbinbin/.vim/swap
 
 # prepare for path
-mkdir -p ~/.vim/bundle/
-cp -R pt.vim ~/.vim/bundle/pt.vim
+mkdir -p /mnt/c/Users/gzzhangbinbin/.vim/bundle/
+cp -R pt.vim /mnt/c/Users/gzzhangbinbin/.vim/bundle/pt.vim
 
 # install dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ~/.vim/dein/
+sh ./installer.sh /mnt/c/Users/gzzhangbinbin/.vim/dein/
