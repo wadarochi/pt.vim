7a2db45a34ffe23972e3e35da3bab20769d34801
PT <wadarochi@gmail.com>
2023-08-21 20:22:33 +0800

    FIX: 还原一些回退是误删的内容

--- script/setup_vim.sh
+++ script/setup_vim.sh
@@ -5,10 +5,19 @@ cp vimrc ~/.vimrc
 mkdir -p ~/.vim/undo
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/swap
+mkdir -p ~/.vim/bookmarks
+mkdir -p ~/.vim/tags
 
 # prepare for path
 mkdir -p ~/.vim/bundle/
 cp -R pt.vim ~/.vim/bundle/pt.vim
+
+# vimwiki html theme
+mkdir -p ~/vimwiki
+mkdir -p ~/vimwiki_html/assets
+mkdir -p ~/vimwiki_html/template
+cp -R assets/* ~/vimwiki_html/assets/
+cp -R template/* ~/vimwiki_html/template/
 
 # install dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
