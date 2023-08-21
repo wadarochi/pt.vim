7556b0d84ae8bb89a7dfd8c028503532f63f536b
PT <wadarochi@gmail.com>
2023-08-21 20:18:29 +0800

    Revert "Windows platform compatibility"
    This reverts commit 93917a4e74b4632da6c2e98361ed4d28bfeff742.

--- script/setup_vim.sh
+++ script/setup_vim.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env sh
 
-cp vimrc /mnt/c/Users/gzzhangbinbin/.vimrc
+cp vimrc ~/.vimrc
 
-mkdir -p /mnt/c/Users/gzzhangbinbin/.vim/undo
-mkdir -p /mnt/c/Users/gzzhangbinbin/.vim/backup
-mkdir -p /mnt/c/Users/gzzhangbinbin/.vim/swap
-mkdir -p /mnt/c/Users/gzzhangbinbin/.vim/bookmarks
-mkdir -p /mnt/c/Users/gzzhangbinbin/.vim/tags
+mkdir -p ~/.vim/undo
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/swap
 
 # prepare for path
-mkdir -p /mnt/c/Users/gzzhangbinbin/.vim/bundle/
-cp -R pt.vim /mnt/c/Users/gzzhangbinbin/.vim/bundle/pt.vim
-
-# vimwiki html theme
-mkdir -p ~/vimwiki
-mkdir -p ~/vimwiki_html/assets
-mkdir -p ~/vimwiki_html/template
-cp -R assets/* ~/vimwiki_html/assets/
-cp -R template/* ~/vimwiki_html/template/
+mkdir -p ~/.vim/bundle/
+cp -R pt.vim ~/.vim/bundle/pt.vim
 
 # install dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh /mnt/c/Users/gzzhangbinbin/.vim/dein/
+sh ./installer.sh ~/.vim/dein/
